b5a20254de71e65a8cb40d05347eeb4b98f69d36
BlackDynamite <rata625@outlook.com>
2021-11-08 21:34:15 -0600

    Initial Commit

---
+++ HBOfficial/HBOfficialApp.swift
@@ -0,0 +1,17 @@
+//
+//  HBOfficialApp.swift
+//  HBOfficial
+//
+//  Created by Robert Taylor-Anderson on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct HBOfficialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
